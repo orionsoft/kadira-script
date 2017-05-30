643942f0a14135ce8312e676a10a4dce4b6dba9b
Robert <robert_zibert@hotmail.com>
2017-05-30 17:00:18 -0400

    adding mongodb to installer

--- install-mongo.sh
+++ install-mongo.sh
@@ -4,6 +4,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
 sudo apt-get update
 sudo apt-get install mongodb-org nano -y
+sudo apt-get install -y mongodb
 sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 
 echo "#################### REGISTERING SERVICE ####################"
