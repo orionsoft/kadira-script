23cfdf10ff3c417fe294d49563c8574213518e7b
Robert <robert_zibert@hotmail.com>
2017-05-30 16:36:54 -0400

    fixing build deplot

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-!/sh ./install-mongo.sh
-!/sh ./install-node.sh
-!/sh ./install-meteor.sh
+echo "INSTALLING MONGO"
+sh /home/deploy/install-mongo.sh
+echo "INSTALLING NODE"
+sh /home/deploy/install-node.sh
+echo "INSTALLING METEOR"
+sh /home/deploy/install-meteor.sh
