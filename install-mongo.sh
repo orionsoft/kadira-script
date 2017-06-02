2b6d6883dec1a85e9fdac276bea9e630fe374b7d
Robert <robert_zibert@hotmail.com>
2017-06-02 12:23:27 -0400

    fixing package installation

--- install-mongo.sh
+++ install-mongo.sh
@@ -2,7 +2,7 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
 sudo apt-get update
-sudo apt-get install mongodb-org mongodb nano -y
+sudo apt-get install mongodb nano -y
 sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
