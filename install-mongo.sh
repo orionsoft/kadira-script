5477d781b42456a9df8445a424d00bb028768496
Robert <robert_zibert@hotmail.com>
2017-06-02 12:57:23 -0400

    fixing mongo starting with rep set

--- install-mongo.sh
+++ install-mongo.sh
@@ -3,6 +3,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
 sudo apt-get -q update
 sudo apt-get -q install mongodb nano -y
+sudo killall mongod
 sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
