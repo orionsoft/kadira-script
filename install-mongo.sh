350ea27391596981b7db26c75f08a8529c4c5db4
Robert <robert_zibert@hotmail.com>
2017-06-02 13:03:29 -0400

    fixing service reference

--- install-mongo.sh
+++ install-mongo.sh
@@ -3,17 +3,17 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
 sudo apt-get -q update
 sudo apt-get -q install mongodb nano -y
-sudo killall mongod
-sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
+sudo rm /etc/systemd/system/mongodb.service
+sudo mv /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
 sudo chown -R mongodb /srv/mongodb/db0
 echo "#################### CHECKING CLUSTER FOLDER CREATION ####################"
 ls /srv/mongodb
 echo "#################### REGISTERING SERVICE ####################"
-sudo systemctl start mongodb
-sudo systemctl status mongodb
-sudo systemctl enable mongodb
+sudo systemctl start mongodb.service
+sudo systemctl status mongodb.service
+sudo systemctl enable mongodb.service
 
 # mongo
 # use kadiraApps
