e858642192e1b5ba24f252859d25faceab125bce
Robert <robert_zibert@hotmail.com>
2017-06-02 11:41:31 -0400

    Silent instalation

--- install-meteor.sh
+++ install-meteor.sh
@@ -1,2 +1,2 @@
-sudo apt-get install python -y
+sudo apt-get install python -y > /dev/null
 curl https://install.meteor.com/ | sh
--- install-mongo.sh
+++ install-mongo.sh
@@ -1,11 +1,12 @@
 # Installing Mongo DB
 echo "#################### INSTALLING MONGO ####################"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
-sudo apt-get update
-sudo apt-get install mongodb-org nano -y
-sudo apt-get install -y mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 > /dev/null 2>&1 &
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y > /dev/null 2>&1 &
+sudo apt-get update > /dev/null
+sudo apt-get install mongodb-org nano -y  > /dev/null 2>&1 &
+sudo apt-get install -y mongodb  > /dev/null 2>&1 &
 sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
+mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
 echo "#################### REGISTERING SERVICE ####################"
 sudo systemctl start mongodb
