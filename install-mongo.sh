9384cf8e52acfe7def73e38befee60207c81b0dc
Robert <robert_zibert@hotmail.com>
2017-06-02 12:48:09 -0400

    fixing mongo installation

--- install-mongo.sh
+++ install-mongo.sh
@@ -1,8 +1,8 @@
 # Installing Mongo DBp
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
-sudo apt-get update
-sudo apt-get install mongodb nano -y
+sudo apt-get -q update
+sudo apt-get -q install mongodb nano -y
 sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
@@ -19,7 +19,7 @@ sudo systemctl enable mongodb
 # db.createUser({ user: "kadira", pwd: "kadira", roles: [ "readWrite", "dbAdmin" ]})
 # use kadiraData
 # db.createUser({ user: "kadira", pwd: "kadira", roles: [ "readWrite", "dbAdmin" ]})
-echo "#################### TESTING MONGO ####################"
+echo "#################### CHECKING MONGO INSTALLATION ####################"
 export LC_ALL=C
-mongo admin --eval 'rs.initiate({_id: "rs0", members:[{_id : 0, host : "localhost:27017"}]})'
+mongo admin --eval 'printjson(rs.initiate({_id: "rs0", members:[{_id : 0, host : "localhost:27017"}]}))'
 mongo admin --eval 'rs.slaveOk()'
