7a4f9f64226f949cb4f00060596f43f984bc8d5c
Robert <robert_zibert@hotmail.com>
2017-06-02 13:11:24 -0400

    fixing details

--- install-mongo.sh
+++ install-mongo.sh
@@ -1,16 +1,17 @@
-# Installing Mongo DBp
+# Installing Mongo DB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
-sudo apt-get -q update
-sudo apt-get -q install mongodb nano -y
+sudo apt-get -qq update
+sudo apt-get -qq install mongodb nano -y
 sudo rm /etc/systemd/system/mongodb.service
-sudo mv /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
+sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
 sudo chown -R mongodb /srv/mongodb/db0
 echo "#################### CHECKING CLUSTER FOLDER CREATION ####################"
 ls /srv/mongodb
 echo "#################### REGISTERING SERVICE ####################"
+systemctl daemon-reload
 sudo systemctl start mongodb.service
 sudo systemctl status mongodb.service
 sudo systemctl enable mongodb.service
--- run-kadira-rma.sh
+++ run-kadira-rma.sh
@@ -20,5 +20,3 @@ sudo mv /home/kadira/deploy/kadira-rma.service /etc/systemd/system/kadira-rma.se
 sudo systemctl start kadira-rma.service
 sudo systemctl status kadira-rma.service
 sudo systemctl enable kadira-rma.service
-
-mongo "mongodb://mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/test?replicaSet=Mycluster0-shard-0" --ssl --username kay --password 'atlasP@$$assw0Rd' --authenticationDatabase admin
