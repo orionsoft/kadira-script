3a08526966faaad9157a0ec6289e8d24351897bd
Robert <robert_zibert@hotmail.com>
2017-06-05 15:14:19 -0400

    little directory error

--- run-kadira-ui.sh
+++ run-kadira-ui.sh
@@ -1,5 +1,5 @@
 # Starting service
-sudo mv /home/deploy/script/kadira-ui.service /etc/systemd/system/kadira_ui.service
+sudo mv /home/deploy/script/kadira-ui.service /etc/systemd/system/kadira-ui.service
 sudo systemctl start kadira-ui.service
 sudo systemctl status kadira-ui.service
 sudo systemctl enable kadira-ui.service
