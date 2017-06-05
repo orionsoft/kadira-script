254001254513615568462fd6c0fc0a3af4661859
Robert <robert_zibert@hotmail.com>
2017-06-05 16:45:34 -0400

    fixing

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 # Here we are going to start the services
 echo "#################### RUNNING KADIRA-UI ####################"
-sh /home/deploy/script/run-kadira-ui.sh
+# sh /home/deploy/script/run-kadira-ui.sh
