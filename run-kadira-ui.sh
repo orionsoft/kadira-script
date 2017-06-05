b892449cc5ecf2ff653ca5eb0ce0a7b7b24d01f6
Robert <robert_zibert@hotmail.com>
2017-06-05 15:29:58 -0400

    saving directories

--- run-kadira-ui.sh
+++ run-kadira-ui.sh
@@ -1,5 +1,5 @@
 # Starting service
-sudo mv /home/deploy/script/kadira-ui.service /etc/systemd/system/kadira-ui.service
+sudo cp /home/deploy/script/kadira-ui.service /etc/systemd/system/kadira-ui.service
 sudo systemctl start kadira-ui.service
 sudo systemctl status kadira-ui.service
 sudo systemctl enable kadira-ui.service
