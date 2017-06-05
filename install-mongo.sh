05496c065a2a507bb977968c3c3c5ac66f1b167f
Robert <robert_zibert@hotmail.com>
2017-06-05 13:25:20 -0400

    adding permissions

--- install-mongo.sh
+++ install-mongo.sh
@@ -9,13 +9,13 @@ ls /etc/systemd/system
 mkdir /srv/mongodb
 mkdir /srv/mongodb/db0
 sudo chown -R mongodb /srv/mongodb/db0
+sudo chown -R $USER /srv/mongodb/db0
 echo "#################### CHECKING CLUSTER FOLDER CREATION ####################"
 ls /srv/mongodb
 echo "#################### REGISTERING SERVICE ####################"
 sudo systemctl daemon-reload
 sudo systemctl stop mongodb.service
 sudo systemctl start mongodb.service
-sudo systemctl status mongodb.service
 sudo systemctl enable mongodb.service
 sudo systemctl status mongodb.service
 # mongo
@@ -25,5 +25,6 @@ sudo systemctl status mongodb.service
 # db.createUser({ user: "kadira", pwd: "kadira", roles: [ "readWrite", "dbAdmin" ]})
 echo "#################### CHECKING MONGO INSTALLATION ####################"
 export LC_ALL=C
-mongo admin --eval 'printjson(rs.initiate({_id: "rs0", members:[{_id : 0, host : "localhost:27017"}]}))'
-mongo admin --eval 'rs.slaveOk()'
+sudo service mongod restart
+mongo --eval 'printjson(rs.initiate({_id: "rs0", members:[{_id : 0, host : "127.0.0.1:27017"}]}))'
+mongo --eval 'rs.slaveOk()'
