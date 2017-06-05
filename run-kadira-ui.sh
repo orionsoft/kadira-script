16343f5c7e01fff2e2b422166f88c67431240f71
Robert <robert_zibert@hotmail.com>
2017-06-05 15:32:49 -0400

    checking ui process

--- run-kadira-ui.sh
+++ run-kadira-ui.sh
@@ -1,5 +1,7 @@
 # Starting service
 sudo cp /home/deploy/script/kadira-ui.service /etc/systemd/system/kadira-ui.service
+ls /home/deploy/script
+ls /etc/systemd/system
 sudo systemctl start kadira-ui.service
 sudo systemctl status kadira-ui.service
 sudo systemctl enable kadira-ui.service
