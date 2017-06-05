18075a4de61460797030f479fc5de88a698850c1
Robert <robert_zibert@hotmail.com>
2017-06-05 15:59:36 -0400

    installing meteor with another user

--- run-kadira-ui.sh
+++ run-kadira-ui.sh
@@ -1,8 +1,9 @@
 # Starting service
+sudo su ubuntu
 cd /home/deploy/app
 source init-shell.sh
 cd kadira-ui
-sudo su - ubuntu -c './run.sh'
+
 
 # sudo cp /home/deploy/script/kadira-ui.service /etc/systemd/system/kadira-ui.service
 # sudo systemctl start kadira-ui.service
