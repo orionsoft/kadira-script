48f5b4339493cfba811e704f34bde7537c88d7b9
Robert <robert_zibert@hotmail.com>
2017-06-05 12:46:56 -0400

    adding changes

--- install-mongo.sh
+++ install-mongo.sh
@@ -3,7 +3,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list -y
 sudo apt-get -qq update
 sudo apt-get -qq install mongodb nano -y
-sudo rm /etc/systemd/system/mongodb.service
 ls /etc/systemd/system
 sudo cp /home/deploy/script/mongodb.service /etc/systemd/system/mongodb.service
 ls /etc/systemd/system
@@ -14,10 +13,11 @@ echo "#################### CHECKING CLUSTER FOLDER CREATION ####################
 ls /srv/mongodb
 echo "#################### REGISTERING SERVICE ####################"
 sudo systemctl daemon-reload
-sudo systemctl restart mongodb.service
+sudo systemctl stop mongodb.service
+sudo systemctl start mongodb.service
 sudo systemctl status mongodb.service
 sudo systemctl enable mongodb.service
-
+sudo systemctl status mongodb.service
 # mongo
 # use kadiraApps
 # db.createUser({ user: "kadira", pwd: "kadira", roles: [ "readWrite", "dbAdmin" ]})
